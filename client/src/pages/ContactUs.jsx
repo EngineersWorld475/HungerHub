5461bb4155560634f950d6dec4830c2f67c59e31
EngineersWorld457 <sanjaygnair777@gmail.com>
2024-09-12 03:56:50 -0700

    some changes in contact us

--- client/src/pages/ContactUs.jsx
+++ client/src/pages/ContactUs.jsx
@@ -46,23 +46,6 @@ const ContactUs = () => {
             </div>
           </div>
         </section>
-
-        <section className="bg-white p-6 rounded-lg shadow-md">
-          <h2 className="text-2xl font-semibold mb-4 text-center">
-            Our Location
-          </h2>
-          <div className="w-full h-60 relative">
-            <iframe
-              title="Google Maps Location"
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3153.210545420644!2d-122.4194186846818!3d37.77492977975924!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8085808c1d5366e9%3A0x9e67e8b0eb7f8e8e!2sSan%20Francisco%2C%20CA!5e0!3m2!1sen!2sus!4v1632097079722!5m2!1sen!2sus"
-              width="100%"
-              height="100%"
-              style={{ border: 0 }}
-              allowFullScreen=""
-              loading="lazy"
-            ></iframe>
-          </div>
-        </section>
       </div>
     </div>
   );
